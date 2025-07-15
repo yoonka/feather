375b90522a3af06e54e4dfc8bc34e2b5371ef5b1
nguthiru <nguthiruedwin@gmail.com>
2025-07-15 18:58:29 +0300

    feat(logging): add file logging backend for production
    Add logger_file_backend dependency and move logging configuration to releases.exs
    This ensures file logging is only enabled in production releases

---
+++ config/releases.exs
@@ -0,0 +1,18 @@
+import Config
+
+config :logger,
+  backends: [{LoggerFileBackend, :error_log},{LoggerFileBackend, :info_log}, {LoggerFileBackend, :console}]
+
+# configuration for the {LoggerFileBackend, :error_log} backend
+config :logger, :error_log,
+  path: "/var/log/feather/error.log",
+  level: :error
+
+# configuration for the {LoggerFileBackend, :info_log} backend
+config :logger, :info_log,
+  path: "/var/log/feather/info.log",
+  level: :info
+
+# configuration for the {LoggerFileBackend, :console} backend
+config :logger, :console,
+  level: :debug
